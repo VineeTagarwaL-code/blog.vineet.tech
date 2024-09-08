424f837e7937662df208d7341206090c026c755c
VineeTagarwaL-code <vineetagarwal.now@gmail.com>
2024-09-08 16:49:07 +0530

    build fix

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: "YourAppNameHere",
+      exec_mode: "cluster",
+      instances: "max", // Or a number of instances
+      script: "./node_modules/next/dist/bin/next",
+      args: "start",
+      exp_backoff_restart_delay: 100, // optional, adjust as needed
+      watch: true, // optional, adjust as needed
+      max_memory_restart: "400M", // optional, adjust as needed
+    },
+  ],
+};
