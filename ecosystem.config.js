ec1711a657aad20037a0cfe46462823548dd6511
VineeTagarwaL-code <vineetagarwal.now@gmail.com>
2024-09-24 01:21:14 +0530

    feat: testing workflow

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "blog.vineet.tech",
+      name: "blog",
       exec_mode: "cluster",
       instances: "max", // Or a number of instances
       script: "./node_modules/next/dist/bin/next",
